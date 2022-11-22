721bf822b3469db6c9c389c448feb0b0afa85017
anelisynagano <48553315+anelisynagano@users.noreply.github.com>
2022-11-22 19:11:07 +0000

    add login/logout functionality

--- public/index.js
+++ public/index.js
@@ -1,19 +1,43 @@
 function Spa() {
+  const [user, setUser] = React.useState({});
+  const [isLoggedIn, setIsLoggedIn] = React.useState(false);
+
   return (
     <HashRouter>
       <div>
-        <UserContext.Provider value={{users:[{name:'abel',email:'abel@mit.edu',password:'secret',balance:100}]}}>
-          <div className="container" style={{padding: "20px"}}>
-            <NavBar/>        
-            <Route path="/" exact component={Home} />
-            <Route path="/CreateAccount/" component={CreateAccount} />
-            <Route path="/login/" component={Login} />
-            <Route path="/logout/" component={Logout} />
-            <Route path="/deposit/" component={Deposit} />
-            <Route path="/withdraw/" component={Withdraw} />
+        <UserContext.Provider
+          value={{
+            users: [
+              {
+                name: 'abel',
+                email: 'abel@mit.edu',
+                password: 'secret',
+                balance: 100,
+              },
+            ],
+          }}
+        >
+          <div className='container' style={{ padding: '20px' }}>
+            <NavBar
+              user={user}
+              isLoggedIn={isLoggedIn}
+              setIsLoggedIn={setIsLoggedIn}
+            />
+            <Route path='/' exact component={Home} />
+            <Route path='/CreateAccount/' component={CreateAccount} />
+            <Route path='/login/'>
+              <Login
+                user={user}
+                setUser={setUser}
+                setIsLoggedIn={setIsLoggedIn}
+              />
+            </Route>
+            <Route path='/logout/' component={Logout} />
+            <Route path='/deposit/' component={Deposit} />
+            <Route path='/withdraw/' component={Withdraw} />
             {/* <Route path="/transactions/" component={Transactions} /> */}
-            <Route path="/balance/" component={Balance} />
-            <Route path="/alldata/" component={AllData} />
+            <Route path='/balance/' component={Balance} />
+            <Route path='/alldata/' component={AllData} />
           </div>
         </UserContext.Provider>
       </div>
@@ -21,7 +45,4 @@ function Spa() {
   );
 }
 
-ReactDOM.render(
-  <Spa/>,
-  document.getElementById('root')
-);
+ReactDOM.render(<Spa />, document.getElementById('root'));
--- public/login.js
+++ public/login.js
@@ -1,77 +1,95 @@
-function Login(){
-  const [show, setShow]     = React.useState(true);
-  const [status, setStatus] = React.useState('');    
+function Login(props) {
+  const [show, setShow] = React.useState(true);
+  const [status, setStatus] = React.useState('');
 
   return (
     <Card
-      bgcolor="info"
-      header="Login"
+      bgcolor='info'
+      header='Login'
       status={status}
-      body={show ? 
-        <LoginForm setShow={setShow} setStatus={setStatus}/> :
-        <LoginMsg setShow={setShow} setStatus={setStatus}/>}
+      body={
+        show ? (
+          <LoginForm
+            setUser={props.setUser}
+            setShow={setShow}
+            setStatus={setStatus}
+            setIsLoggedIn={props.setIsLoggedIn}
+          />
+        ) : (
+          <LoginMsg user={props.user} setShow={setShow} setStatus={setStatus} />
+        )
+      }
     />
-  ) 
+  );
 }
 
-function LoginMsg(props){
-  return(<>
-    <h5>Success</h5>
-    <button type="submit" 
-      className="btn btn-dark" 
-      onClick={() => props.setShow(true)}>
-        Authenticate again
-    </button>
-  </>);
-}
-
-function LoginForm(props){
-  const [email, setEmail]       = React.useState('');
-  const [password, setPassword] = React.useState('');
-  const [isLoggedIn, setIsLoggedIn] = React.useState(false);
+function LoginMsg(props) {
   const ctx = React.useContext(UserContext);
 
-  function handle(){
+  return (
+    <>
+      <h5>Success, welcome {props.user.name}</h5>
+      <button
+        type='submit'
+        className='btn btn-dark'
+        onClick={() => props.setShow(true)}
+      >
+        Authenticate again
+      </button>
+    </>
+  );
+}
+
+function LoginForm(props) {
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+  const ctx = React.useContext(UserContext);
+
+  function handle() {
     fetch(`/account/login/${email}/${password}`)
-    .then(response => response.text())
-    .then(text => {
-      console.log(text);
+      .then((response) => response.text())
+      .then((text) => {
         try {
-            const data = JSON.parse(text);
-            props.setStatus('');
-            props.setShow(false);
-            ctx.users[0].push(data);
-            setIsLoggedIn(true);
-            console.log('JSON:', data);
-        } catch(err) {
-            props.setStatus(text)
-            console.log('err:', text);
+          const data = JSON.parse(text);
+          props.setStatus('');
+          props.setShow(false);
+          ctx.users[0] = data;
+          props.setUser(data);
+          props.setIsLoggedIn(true);
+        } catch (err) {
+          props.setStatus(text);
+          console.log('err:', err);
         }
-    });
+      });
   }
 
-  console.log(ctx);
-  console.log(ctx.users[0]);
-
-  return (<>
-
-    Email<br/>
-    <input type="input" 
-      className="form-control" 
-      placeholder="Enter email" 
-      value={email} 
-      onChange={e => setEmail(e.currentTarget.value)}/><br/>
-
-    Password<br/>
-    <input type="password" 
-      className="form-control" 
-      placeholder="Enter password" 
-      value={password} 
-      onChange={e => setPassword(e.currentTarget.value)}/><br/>
-
-    <button type="submit" className="btn btn-light" onClick={handle}>Login</button>
-   
-  </>);
+  return (
+    <>
+      Email
+      <br />
+      <input
+        type='input'
+        className='form-control'
+        placeholder='Enter email'
+        value={email}
+        onChange={(e) => setEmail(e.currentTarget.value)}
+      />
+      <br />
+      Password
+      <br />
+      <input
+        type='password'
+        className='form-control'
+        placeholder='Enter password'
+        value={password}
+        onChange={(e) => setPassword(e.currentTarget.value)}
+      />
+      <br />
+      <button type='submit' className='btn btn-light' onClick={handle}>
+        Login
+      </button>
+    </>
+  );
 }
 
 //export default function LoginForm({email})
