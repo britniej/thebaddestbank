a2148dc449583226bcb48426ad4a6593fb6009ab
Britnie Roach <70242995+britniej@users.noreply.github.com>
2022-12-05 01:45:32 -1000

    Add files via upload
    reverted back to initial commit

--- dal.js
+++ dal.js
@@ -1,5 +1,5 @@
 const MongoClient = require('mongodb').MongoClient;
-const url         = 'https://thebaddestbank.herokuapp.com';
+const url         = 'mongodb://localhost:27017';
 let db            = null;
  
 // connect to mongo
