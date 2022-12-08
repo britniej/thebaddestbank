6181eb8e1bece57dfd99168b25e2676ae3c7b99c
Britnie Roach <70242995+britniej@users.noreply.github.com>
2022-12-08 02:10:51 -1000

    Add files via upload
    Added new home page and image

---
+++ home.js
@@ -0,0 +1,11 @@
+function Home(){
+  return (
+    <Card
+      txtcolor="black"
+      header="The Baddest Bank Landing Page"
+      title="Welcome to the baddest bank with even worse security"
+      text="Please enjoy this application I have created for my capstone project and do not give any private data in the forms please. Created by Britnie Roach for MITx Pro MERN full stack coding certificate."
+      body={(<img src="badbank.jpg" className="img-fluid" alt="Responsive image"/>)}
+    />
+  );  
+}
